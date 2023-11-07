a979f7567cc81c25e9952cb12f7eda72892a3a9f
Trent <rexzhang68@gmail.com>
2023-11-07 03:11:53 -0600

    responsive nav bar

--- Components/Header.tsx
+++ Components/Header.tsx
@@ -23,8 +23,23 @@ const menu = [
       { name: "List", link: "#list" },
     ],
   },
-  { name: "Me", link: "/user/test", sections: [] },
+  {
+    name: "Me",
+    link: "/user/id",
+    sections: [
+      { name: "Log in", link: "/user/login" },
+      { name: "Profile", link: "/user/profile" },
+      { name: "Log out", link: "/user/logout" },
+      { name: "Sign up", link: "/user/signup" },
+      { name: "Settings", link: "/user/settings" },
+    ],
+  },
 ];
+
+function isPageActive(pathname: string, link: string) {
+  if (link === "/user/id") return pathname.startsWith("/user");
+  else return pathname === link;
+}
 
 export default function Header() {
   const pathname = usePathname();
@@ -33,39 +48,41 @@ export default function Header() {
 
   let hoverTimeout: NodeJS.Timeout;
   return (
-    <motion.div className="w-[40em] z-[1000] fixed top-4 left-1/2 -translate-x-1/2 px-4 py-2 bg-slate-300/30 rounded-full flex justify-between ">
-      <span className="px-4 py-2  text-2xl font-bold">🎥</span>
-      <motion.nav className="flex justify-between items-center">
+    <motion.div className="w-full sm:w-[40em] h-[5em] sm:h-min z-[1000] fixed sm:top-4 left-1/2 -translate-x-1/2 px-4 py-2 bg-slate-300/30 sm:rounded-full flex items-center justify-between gap-3">
+      <span className="  sm:px-4 sm:py-2  text-2xl font-bold">🎥</span>
+      <motion.nav className="flex flex-wrap sm:justify-between items-center text-sm">
         {menu.map((page, index) => (
           //   page
           <motion.div
             layout
             transition={{
               type: "spring",
-              stiffness: 380,
+              stiffness: 320,
               damping: 30,
             }}
             key={index}
-            className={`px-4 py-2  rounded-full flex gap-3 ${
-              activePage === page.link ? "bg-slate-300/50" : "text-white/50"
+            className={`px-2 py-1 sm:px-4 sm:py-2  rounded-full flex gap-3 ${
+              isPageActive(activePage, page.link)
+                ? "bg-slate-300/50"
+                : "text-white/50"
             }`}
             onHoverStart={() => {
               setActivePage(page.link);
             }}
-            onHoverEnd={() => {
-              setActivePage(pathname);
-            }}
+            // onHoverEnd={() => {
+            //   setActivePage(pathname);
+            // }}
           >
-            <motion.div>
+            <motion.div layout>
               <Link href={page.link}>
                 <span>{page.name}</span>
               </Link>
             </motion.div>
 
-            {activePage === page.link && (
+            {isPageActive(activePage, page.link) && (
               <motion.div
                 transition={{ duration: 4 }}
-                className={"flex justify-end gap-2 "}
+                className={"flex flex-wrap justify-end gap-2 "}
               >
                 {page.sections.map((section, index) => (
                   //section
