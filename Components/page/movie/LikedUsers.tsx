a4c33f4c5ecd752526c76b0348f5f48bda5bb981
Trent <rexzhang68@gmail.com>
2023-11-29 21:21:27 -0600

    next ui

--- Components/page/movie/LikedUsers.tsx
+++ Components/page/movie/LikedUsers.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-// import { Avatar, Tooltip } from "@nextui-org/react";
+import { Avatar, Tooltip } from "@nextui-org/react";
 
 const renderTooltip = (props: any) => (
   <div id="button-tooltip" {...props}>
@@ -22,23 +22,21 @@ export function LikedUsers({ liked_users }: { liked_users: LikedUsersProps }) {
   return (
     <div className="flex flex-wrap ">
       {liked_users[0]
-        ? liked_users.map(
-            (user, index) =>
-              // <Tooltip
-              //   key={index}
-              //   showArrow={true}
-              //   content={
-              //     <div className="flex flex-col items-center justify-center text-black">
-              //       <div>{user.username}</div>
-              //     </div>
-              //   }
-              // >
-              //   <Link href={`/user/${user.email}`}>
-              //     <Avatar isBordered src={genderDefaultAvater(user.gender)} />
-              //   </Link>
-              // </Tooltip>
-              "d"
-          )
+        ? liked_users.map((user, index) => (
+            <Tooltip
+              key={index}
+              showArrow={true}
+              content={
+                <div className="flex flex-col items-center justify-center text-black">
+                  <div>{user.username}</div>
+                </div>
+              }
+            >
+              <Link href={`/user/${user.email}`}>
+                <Avatar isBordered src={genderDefaultAvater(user.gender)} />
+              </Link>
+            </Tooltip>
+          ))
         : "No one liked this movie yet"}
     </div>
   );
